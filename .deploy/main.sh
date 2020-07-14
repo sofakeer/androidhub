ce1721741ef8deb117e205e14b54d2b71fced459
angfangshuo <sofakeer@gmail.com>
2020-07-14 17:17:08 +0800

    update

--- .deploy/main.sh
+++ .deploy/main.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-WEB_PATH='/root/mynode'
+WEB_PATH=$(dirname $0)
 # WEB_USER='root'
 # WEB_USERGROUP='root'
 
-echo "[deploy] Start deployment..."
+echo "[deploy] Start deployment..." 
  
-echo "[deploy] Fetch and rebuilding..."
+echo "[deploy] path: " $WEB_PATH
+
 cd $WEB_PATH
-cd ..
+
 echo "[deploy] path:" $(pwd)
 echo "[deploy] pulling source code..."
 git fetch --all && git reset --hard origin/master && git pull
@@ -23,9 +24,6 @@ npm i --production
 
 echo "[deploy] Stop service..."
 npm run assets
-
-echo "[deploy] Remove old dist..."
-yarn prebuild
 
 echo "[deploy] Stoping..."
 npm stop
