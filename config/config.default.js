1c78214a485920ef7ab07b26d4adcf04bf5c1504
angfangshuo <sofakeer@gmail.com>
2020-07-23 16:26:38 +0800

    fix error fuck

--- config/config.default.js
+++ config/config.default.js
@@ -1,66 +1,66 @@
-"use strict";
-const path = require("path");
+'use strict';
+const path = require('path');
 
-module.exports = (appInfo) => {
+module.exports = appInfo => {
   const config = {};
 
   config.proxy = true;
 
-  config.name = "Android技术社区";
+  config.name = 'Android技术社区';
 
-  config.google_tracker_id = "UA-98661850-1";
+  config.google_tracker_id = 'UA-98661850-1';
 
-  config.description = "Android专业中文社区";
+  config.description = 'Android专业中文社区';
 
-  config.site_logo = "/public/images/cnodejs_light.svg";
+  config.site_logo = '/public/images/cnodejs_light.svg';
 
-  config.site_icon = "/public/images/cnode_icon_32.png";
+  config.site_icon = '/public/images/cnode_icon_32.png';
 
   // debug 为 true 时，用于本地调试
   config.debug = true;
 
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + "_1519887194138_3450";
+  config.keys = appInfo.name + '_1519887194138_3450';
 
-  config.host = "http://cnodejs.org";
+  config.host = 'http://cnodejs.org';
 
-  config.session_secret = "node_club_secret"; // 务必修改
+  config.session_secret = 'node_club_secret'; // 务必修改
 
   // add your config here
   config.middleware = [
-    "locals",
-    "authUser",
-    "blockUser",
-    "errorPage",
-    "errorHandler",
+    'locals',
+    'authUser',
+    'blockUser',
+    'errorPage',
+    'errorHandler',
   ];
 
   config.authUser = {
     enable: true,
-    match: "/",
+    match: '/',
   };
 
   // 是否允许直接注册（否则只能走 github 的方式）
   config.allow_sign_up = false;
 
   // cdn host，如 http://cnodejs.qiniudn.com
-  config.site_static_host = process.env.EGG_SITE_STATIC_HOST || ""; // 静态文件存储域名
+  config.site_static_host = process.env.EGG_SITE_STATIC_HOST || ''; // 静态文件存储域名
 
   config.mini_assets = process.env.EGG_MINI_ASSETS || false;
 
   // 版块
   config.tabs = [
-    ["share", "分享"],
-    ["ask", "问答"],
-    ["job", "招聘"],
+    [ 'share', '分享' ],
+    [ 'ask', '问答' ],
+    [ 'job', '招聘' ],
   ];
 
   // RSS配置
   config.rss = {
-    title: "CNode：Node.js专业中文社区",
-    link: "http://cnodejs.org",
-    language: "zh-cn",
-    description: "CNode：Node.js专业中文社区",
+    title: 'CNode：Node.js专业中文社区',
+    link: 'http://cnodejs.org',
+    language: 'zh-cn',
+    description: 'CNode：Node.js专业中文社区',
     // 最多获取的RSS Item数量
     max_rss_items: 50,
   };
@@ -69,59 +69,59 @@ module.exports = (appInfo) => {
 
   // 7牛的access信息，用于文件上传
   config.qn_access = {
-    accessKey: "your access key",
-    secretKey: "your secret key",
-    bucket: "your bucket name",
-    origin: "http://your qiniu domain",
+    accessKey: 'your access key',
+    secretKey: 'your secret key',
+    bucket: 'your bucket name',
+    origin: 'http://your qiniu domain',
     // 如果vps在国外，请使用 http://up.qiniug.com/ ，这是七牛的国际节点
     // 如果在国内，此项请留空
-    uploadURL: "http://xxxxxxxx",
+    uploadURL: 'http://xxxxxxxx',
   };
 
   // 文件上传配置
   // 注：如果填写 qn_access，则会上传到 7牛，以下配置无效
   config.upload = {
-    path: path.join(__dirname, "../app/public/upload/"),
-    url: "/public/upload/",
+    path: path.join(__dirname, '../app/public/upload/'),
+    url: '/public/upload/',
   };
 
   config.view = {
-    defaultViewEngine: "ejs",
+    defaultViewEngine: 'ejs',
     mapping: {
-      ".html": "ejs",
+      '.html': 'ejs',
     },
   };
 
   config.ejs = {
-    layout: "layout.html",
+    layout: 'layout.html',
   };
 
   config.avatars_allow_hostname = [
-    "avatars0.githubusercontent.com",
-    "avatars1.githubusercontent.com",
-    "avatars2.githubusercontent.com",
-    "avatars.githubusercontent.com",
-    "www.gravatar.com",
-    "gravatar.com",
-    "www.google-analytics.com",
+    'avatars0.githubusercontent.com',
+    'avatars1.githubusercontent.com',
+    'avatars2.githubusercontent.com',
+    'avatars.githubusercontent.com',
+    'www.gravatar.com',
+    'gravatar.com',
+    'www.google-analytics.com',
   ];
 
-  config.auth_cookie_name = "android_hub";
+  config.auth_cookie_name = 'android_hub';
   config.admins = {
     ADMIN_USER: true,
   };
 
   config.siteFile = {
-    "/favicon.ico": "/public/images/cnode_icon_32.png",
+    '/favicon.ico': '/public/images/cnode_icon_32.png',
   };
 
   // database
   config.redis = {
     client: {
-      host: process.env.EGG_REDIS_HOST || "127.0.0.1",
+      host: process.env.EGG_REDIS_HOST || '127.0.0.1',
       port: process.env.EGG_REDIS_PORT || 6379,
-      password: process.env.EGG_REDIS_PASSWORD || "",
-      db: process.env.EGG_REDIS_DB || "0",
+      password: process.env.EGG_REDIS_PASSWORD || '',
+      db: process.env.EGG_REDIS_DB || '0',
     },
   };
 
@@ -129,7 +129,7 @@ module.exports = (appInfo) => {
    * @see http://mongodb.github.io/node-mongodb-native/2.2/api/Db.html#createCollection
    */
   config.mongoose = {
-    url: process.env.EGG_MONGODB_URL || "mongodb://127.0.0.1:27017/egg_cnode",
+    url: process.env.EGG_MONGODB_URL || 'mongodb://127.0.0.1:27017/egg_cnode',
     options: {
       server: { poolSize: 20 },
       reconnectTries: 10,
@@ -139,33 +139,33 @@ module.exports = (appInfo) => {
 
   // passport
   config.passportGithub = {
-    callbackURL: "/passport/github/callback",
-    key: "6f07adc7c5b92b54ef29",
-    secret: "b2731bc7d7c995a4dadd2fb5161ef4c8fdd2ee8d",
+    callbackURL: '/passport/github/callback',
+    key: '6f07adc7c5b92b54ef29',
+    secret: 'b2731bc7d7c995a4dadd2fb5161ef4c8fdd2ee8d',
   };
 
   config.passportLocal = {
-    usernameField: "name",
-    passwordField: "pass",
+    usernameField: 'name',
+    passwordField: 'pass',
   };
 
   // 邮箱配置
   config.mail_opts = {
-    host: "smtp.126.com",
+    host: 'smtp.126.com',
     port: 25,
     auth: {
-      user: "club@126.com",
-      pass: "club",
+      user: 'club@126.com',
+      pass: 'club',
     },
     ignoreTLS: true,
   };
 
   config.alinode = {
     // 从 `Node.js 性能平台` 获取对应的接入参数
-    appid: process.env.EGG_ALINODE_APPID || "85578",
+    appid: process.env.EGG_ALINODE_APPID || '85578',
     secret:
       process.env.EGG_ALINODE_SECRET ||
-      "97f905e2ebbc50231f97188969ffbfd8d03c9dfd",
+      '97f905e2ebbc50231f97188969ffbfd8d03c9dfd',
   };
 
   config.topic = {
@@ -177,11 +177,11 @@ module.exports = (appInfo) => {
   // 每个 IP 每天可创建用户数
   config.create_user_per_ip = 1000;
 
-  config.search = "google"; // 'google', 'baidu', 'local'
+  config.search = 'google'; // 'google', 'baidu', 'local'
 
   config.security = {
     csrf: {
-      ignore: "/api/*/*",
+      ignore: '/api/*/*',
     },
   };
 
